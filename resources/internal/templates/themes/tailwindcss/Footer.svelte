f32950c3f0605cf6c6121782cf6dc70b13684b41
indaco <github@mircoveltri.me>
2022-01-16 18:54:34 +0100

    defaults components added

---
+++ resources/internal/templates/themes/tailwindcss/Footer.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { sveltinVersion } from '$config/defaults.js';
+	import type { WebSite } from '@sveltinio/seo/types';
+
+	export let websiteData: WebSite;
+</script>
+
+<footer>
+	<div class="content">
+		<div class="wrapper">
+			<div>
+				<a href="/">
+					<img
+						src="{websiteData.baseURL}/{websiteData.logo}"
+						alt="sveltin logo"
+						style="width: 100px; height: 48px;"
+					/>
+					<span class="sr-only">Sveltin Logo</span>
+				</a>
+			</div>
+			<p>
+				The CLI to Boost Your Productivity creating SvelteKit powered
+				static websites.
+			</p>
+			<p><strong>You are running Sveltin v{sveltinVersion}</strong></p>
+		</div>
+	</div>
+</footer>
---
+++ resources/internal/templates/themes/tailwindcss/Hero.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { ExternalLink } from '@sveltinio/essentials';
+</script>
+
+<section class="hero">
+	<div class="content">
+		<div class="wrapper">
+			<div class="subwrapper">
+				<div class="herotext">
+					<h1>
+						Your Sveltin project is <span class="accent"
+							>up & running!</span
+						>
+					</h1>
+
+					<div class="buttons-wrapper">
+						<ExternalLink
+							id="learnMoreLink"
+							name="learnMore"
+							url="https://docs.sveltin.io/tutorials"
+							alt="link to the tutorial pages"
+							class="btn btn-accent">Learn More</ExternalLink
+						>
+
+						<ExternalLink
+							id="documentationLink"
+							name="docs"
+							url="https://docs.sveltin.io"
+							alt="link to the documentation website"
+							class="btn btn-base">Read the Docs</ExternalLink
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
