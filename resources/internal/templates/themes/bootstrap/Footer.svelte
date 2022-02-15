80e1fa7b974be5e8a5a93a6667159917c2fcfe55
indaco <github@mircoveltri.me>
2022-02-15 11:29:34 +0100

    Bootstrap 5 support implemented

---
+++ resources/internal/templates/themes/bootstrap/Footer.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import { sveltinVersion } from '$config/defaults.js';
+	import type { IWebSite } from '@sveltinio/seo/types';
+
+	export let websiteData: IWebSite;
+</script>
+
+<footer class="lh-base bg-dark text-muted" style="line-height: 1.25rem;">
+	<div class="wrapper w-100 mx-auto">
+		<div class="flex-shrink-0 text-start w-xxs">
+			<div>
+				<a href="/">
+					<img
+						src="{websiteData.baseURL}/{websiteData.logo}"
+						alt="sveltin logo"
+						style="width: 100px; height: 48px;"
+					/>
+					<span class="visually-hidden">Sveltin Logo</span>
+				</a>
+			</div>
+			<p class="mt-5">
+				The CLI to Boost Your Productivity creating SvelteKit powered
+				static websites.
+			</p>
+			<p class="mt-5">
+				<strong>You are running Sveltin v{sveltinVersion}</strong>
+			</p>
+		</div>
+	</div>
+</footer>
+
+<style lang="scss">
+	p {
+		font-size: $font-size-sm;
+	}
+
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		flex-wrap: wrap;
+		padding-left: 1.25rem /* 20px */;
+		padding-right: 1.25rem /* 20px */;
+		padding-top: 2rem /* 32px */;
+		padding-bottom: 1.5rem /* 24px */;
+	}
+
+	@media (min-width: 640px) {
+		.wrapper {
+			padding-left: 1.5rem;
+			padding-right: 1.5rem;
+		}
+	}
+
+	@media (min-width: 768px) {
+		.wrapper {
+			flex-direction: row;
+			align-items: center;
+		}
+	}
+
+	@media (min-width: 1024px) {
+		.wrapper {
+			padding-left: 3rem /* 48px */;
+			padding-right: 3rem /* 48px */;
+			padding-top: 3rem /* 48px */;
+			padding-bottom: 2.25rem /* 48px */;
+			align-items: flex-start;
+			flex-wrap: nowrap;
+		}
+	}
+</style>
--- sveltinlib/css/bootstrap.go
+++ sveltinlib/css/bootstrap.go
@@ -8,16 +8,70 @@ package css
 
 import (
 	"embed"
+	"path/filepath"
 
 	"github.com/spf13/afero"
 	"github.com/sveltinio/sveltin/config"
-	"github.com/sveltinio/sveltin/sveltinlib/sveltinerr"
+	"github.com/sveltinio/sveltin/helpers"
+	"github.com/sveltinio/sveltin/resources"
 )
 
 type Bootstrap struct {
 	CSSLib
 }
 
-func (f *Bootstrap) init(efs *embed.FS, fs afero.Fs, conf *config.SveltinConfig, projectName string, npmClientName string, themeName string) error {
-	return sveltinerr.NewNotImplementYetError()
+func (f *Bootstrap) init(efs *embed.FS, fs afero.Fs, conf *config.SveltinConfig, tplData *config.TemplateData) error {
+	// Copying the package.json config file
+	sourceFile := resources.SveltinBootstrapCSSThemeFS["package_json"]
+	template := helpers.BuildTemplate(sourceFile, nil, tplData)
+	content := template.Run(efs)
+	saveAs := filepath.Join(conf.GetProjectRoot(), tplData.ProjectName, "package.json")
+	if err := helpers.WriteContentToDisk(fs, saveAs, content); err != nil {
+		return err
+	}
+	// Copying __layout.svelte. file
+	sourceFile = resources.SveltinBootstrapCSSThemeFS["layout"]
+	template = helpers.BuildTemplate(sourceFile, nil, tplData)
+	content = template.Run(efs)
+	saveAs = filepath.Join(conf.GetProjectRoot(), tplData.ProjectName, "src", "routes", "__layout.svelte")
+	if err := helpers.WriteContentToDisk(fs, saveAs, content); err != nil {
+		return err
+	}
+	// Copying app.html file
+	sourceFile = resources.SveltinBootstrapCSSThemeFS["app_html"]
+	saveAs = filepath.Join(conf.GetProjectRoot(), tplData.ProjectName, "src", "app.html")
+	if err := f.copyConfigFiles(efs, fs, sourceFile, saveAs, true); err != nil {
+		return err
+	}
+	// Copying app.scss file
+	sourceFile = resources.SveltinBootstrapCSSThemeFS["app_css"]
+	saveAs = filepath.Join(conf.GetProjectRoot(), tplData.ProjectName, "src", "app.scss")
+	if err := f.copyConfigFiles(efs, fs, sourceFile, saveAs, true); err != nil {
+		return err
+	}
+	// Copying variables.scss file
+	sourceFile = resources.SveltinBootstrapCSSThemeFS["variables_scss"]
+	saveAs = filepath.Join(conf.GetProjectRoot(), tplData.ProjectName, "src", "variables.scss")
+	if err := f.copyConfigFiles(efs, fs, sourceFile, saveAs, true); err != nil {
+		return err
+	}
+	// Copying svelte.config.js file
+	sourceFile = resources.SveltinBootstrapCSSThemeFS["svelte_config"]
+	saveAs = filepath.Join(conf.GetProjectRoot(), tplData.ProjectName, "svelte.config.js")
+	if err := f.copyConfigFiles(efs, fs, sourceFile, saveAs, true); err != nil {
+		return err
+	}
+	// Copying Hero.svelte component
+	sourceFile = resources.SveltinBootstrapCSSThemeFS["hero"]
+	saveAs = filepath.Join(conf.GetProjectRoot(), tplData.ProjectName, "themes", tplData.ThemeName, "partials", "Hero.svelte")
+	if err := f.copyConfigFiles(efs, fs, sourceFile, saveAs, true); err != nil {
+		return err
+	}
+	// Copying Footer.svelte component
+	sourceFile = resources.SveltinBootstrapCSSThemeFS["footer"]
+	saveAs = filepath.Join(conf.GetProjectRoot(), tplData.ProjectName, "themes", tplData.ThemeName, "partials", "Footer.svelte")
+	if err := f.copyConfigFiles(efs, fs, sourceFile, saveAs, true); err != nil {
+		return err
+	}
+	return nil
 }
