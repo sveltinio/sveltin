2d5d6701c4dc4998f4bf661da916518b1fa78511
indaco <github@mircoveltri.me>
2022-02-22 11:25:33 +0100

    refactor(next_steps.go): interpolate multiline strings

--- common/next_steps.go
+++ common/next_steps.go
@@ -1,8 +1,13 @@
 package common
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func HelperTextNewProject(projectName string) string {
-	return `
-   1. cd ` + projectName + `
+	placeHolderText := `
+   1. cd %s
    2. sveltin prepare (or sveltin init, etc)
    3. sveltin server
 
@@ -10,18 +15,40 @@ To close the dev server, hit Ctrl-C
 
 Visit the Quick Start guide at https://docs.sveltin.io/quick-start
 `
+	return fmt.Sprintf(placeHolderText, projectName)
 }
 
 func HelperTextNewResource(resourceName string) string {
-	return `Your resource is ready to be used. Start by adding content to it.
+	placeHolderText := `Your resource is ready to be used. Start by adding content to it.
 
-Eg: sveltin new content ` + resourceName + `/getting-started
+Eg: sveltin new content %s /getting-started
 `
+	return fmt.Sprintf(placeHolderText, resourceName)
 }
 
 func HelperTextNewMetadata(metadataName string) string {
-	return `Your metadata ` + metadataName + ` is ready to be used.
+	placeHolderText := `Your metadata %s is ready to be used.
 
 Ensure your markdown frontmatter consider it.
 `
+	return fmt.Sprintf(placeHolderText, metadataName)
+}
+
+func HelperTextDryRunFlag() string {
+	return `  * *********************** *
+  * RUNNING IN DRY-RUN MODE *
+  * *********************** *`
+}
+
+func HelperTextDeploySummary(numOfFolders, numOfFiles int) string {
+	placeHolderText := `
+SUMMARY:
+
+- Total number of created folders: %s
+- Total number of copied files: %s`
+
+	folderCounter := strconv.Itoa(numOfFolders)
+	filesCounter := strconv.Itoa(numOfFiles)
+
+	return fmt.Sprintf(placeHolderText, folderCounter, filesCounter)
 }
