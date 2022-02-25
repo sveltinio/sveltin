fd311cac78ca56cfa3f776418a59c1c88350be5d
indaco <github@mircoveltri.me>
2022-02-25 18:45:50 +0100

    docs: code comments

--- sveltinlib/fsm/sveltin_fsm.go
+++ sveltinlib/fsm/sveltin_fsm.go
@@ -1,9 +1,11 @@
-/*
-Copyright © 2021 Mirco Veltri <github@mircoveltri.me>
+/**
+ * Copyright © 2021 Mirco Veltri <github@mircoveltri.me>
+ *
+ * Use of this source code is governed by Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
 
-Use of this source code is governed by Apache 2.0 license
-that can be found in the LICENSE file.
-*/
+// Package fsm ...
 package fsm
 
 import (
@@ -16,16 +18,19 @@ import (
 	"github.com/sveltinio/sveltin/sveltinlib/pathmaker"
 )
 
+// SveltinFSManager is the struct for a pathmaker.
 type SveltinFSManager struct {
 	maker *pathmaker.SveltinPathMaker
 }
 
+// NewSveltinFSManager returns a pointer to a SveltinFSManager struct.
 func NewSveltinFSManager(maker *pathmaker.SveltinPathMaker) *SveltinFSManager {
 	return &SveltinFSManager{
 		maker: maker,
 	}
 }
 
+// GetFolder returns a Folder struct for the provided folder name.
 func (s *SveltinFSManager) GetFolder(name string) *composer.Folder {
 	switch name {
 	case "root":
@@ -49,10 +54,12 @@ func (s *SveltinFSManager) GetFolder(name string) *composer.Folder {
 	}
 }
 
+// NewResourceContentFolder returns a pointer to the 'resource content' Folder.
 func (s *SveltinFSManager) NewResourceContentFolder(name string, resource string) *composer.Folder {
 	return composer.NewFolder(filepath.Join(resource, name))
 }
 
+// NewResourceContentFile returns a pointer to the 'resource content' File.
 func (s *SveltinFSManager) NewResourceContentFile(name string, template string) *composer.File {
 	return &composer.File{
 		Name:       s.maker.GetResourceContentFilename(),
@@ -63,6 +70,7 @@ func (s *SveltinFSManager) NewResourceContentFile(name string, template string) 
 	}
 }
 
+// NewPublicPage returns a pointer to a new 'public page' File.
 func (s *SveltinFSManager) NewPublicPage(name string, language string) *composer.File {
 	return &composer.File{
 		Name:       helpers.PublicPageFilename(name, language),
@@ -73,6 +81,7 @@ func (s *SveltinFSManager) NewPublicPage(name string, language string) *composer
 	}
 }
 
+// NewNoPage returns a pointer to a 'no-public page' File.
 func (s *SveltinFSManager) NewNoPage(name string, projectConfig *config.ProjectConfig, resources []string, contents map[string][]string, metadata map[string][]string, pages []string) *composer.File {
 	return &composer.File{
 		Name:       name + ".xml",
@@ -86,6 +95,7 @@ func (s *SveltinFSManager) NewNoPage(name string, projectConfig *config.ProjectC
 	}
 }
 
+// NewConfigFile returns a pointer to a new 'config' File.
 func (s *SveltinFSManager) NewConfigFile(projectName string, name string, cliVersion string) *composer.File {
 	filename := strings.ToLower(name) + ".js.ts"
 	return &composer.File{
@@ -99,6 +109,7 @@ func (s *SveltinFSManager) NewConfigFile(projectName string, name string, cliVer
 	}
 }
 
+// NewDotEnvFile returns a pointer to a new '.env' File.
 func (s *SveltinFSManager) NewDotEnvFile(projectName string, tplData *config.TemplateData) *composer.File {
 	return &composer.File{
 		Name:         tplData.Name,
@@ -107,6 +118,7 @@ func (s *SveltinFSManager) NewDotEnvFile(projectName string, tplData *config.Tem
 	}
 }
 
+// NewContentFile returns a pointer to a new 'content' File.
 func (s *SveltinFSManager) NewContentFile(name string, template string, resource string) *composer.File {
 	return &composer.File{
 		Name:       s.maker.GetResourceContentFilename(),
