ec699a8d662baf622efa32db4f0cc789d2d52daa
indaco <github@mircoveltri.me>
2022-03-11 14:32:50 +0100

    feature: sveltekit version and build time exposed to the info.json endpoint

--- resources/internal/templates/themes/vanillacss/svelte.config.js
+++ resources/internal/templates/themes/vanillacss/svelte.config.js
@@ -1,4 +1,10 @@
 import path from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
@@ -30,6 +36,14 @@ const config = {
 			entries: ['*'],
 		},
 		vite: {
+			define: {
+				'process.env.VITE_SVELTEKIT_VERSION': JSON.stringify(
+					String(pkg.devDependencies['@sveltejs/kit'])
+				),
+				'process.env.VITE_BUILD_TIME': JSON.stringify(
+					new Date().toISOString()
+				),
+			},
 			server: {
 				fs: {
 					// Allow serving files from one level up to the project root
