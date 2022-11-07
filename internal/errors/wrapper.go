8210293a0633d18b8d42253a72764cd2df04c6d6
Mirco Veltri <github@mircoveltri.me>
2022-11-07 15:09:35 +0100

    chore(sveltinerr): package comment string

--- internal/errors/wrapper.go
+++ internal/errors/wrapper.go
@@ -5,7 +5,7 @@
  * that can be found in the LICENSE file.
  */
 
-// Package sveltinerr ...
+// Package sveltinerr contains all the utility functions to map errors in Sveltin.
 package sveltinerr
 
 import (
