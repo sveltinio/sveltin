da589e8ee81083db8f748173be603fe1992f595e
Mirco Veltri <github@mircoveltri.me>
2022-11-08 14:02:03 +0100

    feat: theme config migration added to the upgrade command

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"path"
 
+	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	"github.com/sveltinio/sveltin/internal/markup"
 	"github.com/sveltinio/sveltin/internal/migrations"
@@ -23,6 +24,7 @@ import (
 const (
 	ProjectSettingsMigrationID string = "projectSettings"
 	DefaultsConfigMigrationID  string = "defaultsConfig"
+	ThemeConfigMigrationID     string = "themeConfig"
 )
 
 //=============================================================================
@@ -47,14 +49,22 @@ func RunUpgradeCmd(cmd *cobra.Command, args []string) {
 	cfg.log.Plain(markup.H1(fmt.Sprintf("Upgrading your project to sveltin v%s", CliVersion)))
 	migrationManager := migrations.NewMigrationManager()
 
+	// FILE: <project_root>/sveltin.config.json
 	pathToProjectSettingsFile := path.Join(cwd, ProjectSettingsFile)
-	addProjectSettingsMigration := handleMigration(ProjectSettingsMigrationID, migrationManager, pathToProjectSettingsFile)
+	addProjectSettingsMigration := handleMigration(ProjectSettingsMigrationID, migrationManager, cfg, pathToProjectSettingsFile)
 	err := addProjectSettingsMigration.Execute()
 	utils.ExitIfError(err)
 
+	// FILE: <project_root>/config/defaults.js.ts
 	pathToDefaultsConfigFile := path.Join(cwd, cfg.pathMaker.GetConfigFolder(), DefaultsConfigFile)
-	updateDefaultsConfigMigration := handleMigration(DefaultsConfigMigrationID, migrationManager, pathToDefaultsConfigFile)
+	updateDefaultsConfigMigration := handleMigration(DefaultsConfigMigrationID, migrationManager, cfg, pathToDefaultsConfigFile)
 	err = updateDefaultsConfigMigration.Execute()
+	utils.ExitIfError(err)
+
+	// FILE: <project_root>/themes/<theme_name>/theme.config.js
+	pathToThemeConfigFile := path.Join(cwd, cfg.pathMaker.GetThemesFolder(), retrieveThemeName(cfg), cfg.settings.GetThemeConfigFilename())
+	updateThemeConfigMigration := handleMigration(ThemeConfigMigrationID, migrationManager, cfg, pathToThemeConfigFile)
+	err = updateThemeConfigMigration.Execute()
 	utils.ExitIfError(err)
 
 	cfg.log.Success(fmt.Sprintf("Your project is ready for sveltin v%s\n", CliVersion))
@@ -66,43 +76,75 @@ func init() {
 
 //=============================================================================
 
-func handleMigration(migrationType string, migrationManager *migrations.MigrationManager, pathToFile string) migrations.Migration {
+func handleMigration(migrationType string, migrationManager *migrations.MigrationManager, config appConfig, pathToFile string) migrations.Migration {
 	switch migrationType {
 	case ProjectSettingsMigrationID:
-		return newAddProjectSettingsMigration(migrationManager, pathToFile)
+		return newAddProjectSettingsMigration(migrationManager, config, pathToFile)
 	case DefaultsConfigMigrationID:
-		return newUpdateDefaultsConfigMigration(migrationManager, pathToFile)
+		return newUpdateDefaultsConfigMigration(migrationManager, config, pathToFile)
+	case ThemeConfigMigrationID:
+		return newUpdateThemeConfigMigration(migrationManager, config, pathToFile)
 	default:
 		return nil
 	}
 }
 
-func newAddProjectSettingsMigration(migrationManager *migrations.MigrationManager, pathTofile string) *migrations.AddProjectSettingsMigration {
+func newAddProjectSettingsMigration(migrationManager *migrations.MigrationManager, config appConfig, pathTofile string) *migrations.AddProjectSettingsMigration {
 	return &migrations.AddProjectSettingsMigration{
 		Mediator:  migrationManager,
-		Fs:        cfg.fs,
-		FsManager: cfg.fsManager,
-		PathMaker: cfg.pathMaker,
-		Logger:    cfg.log,
+		Fs:        config.fs,
+		FsManager: config.fsManager,
+		PathMaker: config.pathMaker,
+		Logger:    config.log,
 		Data: &migrations.MigrationData{
 			PathToFile:        pathTofile,
 			CliVersion:        CliVersion,
-			ProjectCliVersion: cfg.projectSettings.CLI.Version,
+			ProjectCliVersion: config.projectSettings.CLI.Version,
 		},
 	}
 }
 
-func newUpdateDefaultsConfigMigration(migrationManager *migrations.MigrationManager, pathTofile string) *migrations.UpdateDefaultsConfigMigration {
+func newUpdateDefaultsConfigMigration(migrationManager *migrations.MigrationManager, config appConfig, pathTofile string) *migrations.UpdateDefaultsConfigMigration {
 	return &migrations.UpdateDefaultsConfigMigration{
 		Mediator:  migrationManager,
-		Fs:        cfg.fs,
-		FsManager: cfg.fsManager,
-		PathMaker: cfg.pathMaker,
-		Logger:    cfg.log,
+		Fs:        config.fs,
+		FsManager: config.fsManager,
+		PathMaker: config.pathMaker,
+		Logger:    config.log,
 		Data: &migrations.MigrationData{
 			PathToFile:        pathTofile,
 			CliVersion:        CliVersion,
-			ProjectCliVersion: cfg.projectSettings.CLI.Version,
+			ProjectCliVersion: config.projectSettings.CLI.Version,
 		},
 	}
+}
+
+func newUpdateThemeConfigMigration(migrationManager *migrations.MigrationManager, config appConfig, pathTofile string) *migrations.UpdateThemeConfigMigration {
+	return &migrations.UpdateThemeConfigMigration{
+		Mediator:  migrationManager,
+		Fs:        config.fs,
+		FsManager: config.fsManager,
+		PathMaker: config.pathMaker,
+		Logger:    config.log,
+		Data: &migrations.MigrationData{
+			PathToFile:        pathTofile,
+			CliVersion:        CliVersion,
+			ProjectCliVersion: config.projectSettings.CLI.Version,
+		},
+	}
+}
+
+//=============================================================================
+
+func retrieveThemeName(config appConfig) string {
+	files, err := afero.ReadDir(config.fs, config.pathMaker.GetThemesFolder())
+	utils.ExitIfError(err)
+
+	var themeName string
+	for _, file := range files {
+		if file.IsDir() {
+			themeName = file.Name()
+		}
+	}
+	return themeName
 }
