6b072f357f741a8e7e84ead9574644a92816572e
Mirco Veltri <github@mircoveltri.me>
2022-11-21 09:44:21 +0100

    refactor(add content cmd): uniforming how cmd works compared to others

--- internal/tpltypes/content.go
+++ internal/tpltypes/content.go
@@ -7,9 +7,29 @@
 
 package tpltypes
 
+const (
+	// Blank represents the fontmatter-only template id used when generating the content file.
+	Blank string = "blank"
+	// Sample represents the sample-content template id used when generating the content file.
+	Sample string = "sample"
+)
+
 // ContentData is the struct representing the user selection for new content.
 type ContentData struct {
 	Name     string
 	Resource string
 	Type     string
+}
+
+// NewContentData creates a pointer to a ContentData struct.
+func NewContentData(name, cResource string, isSample bool) *ContentData {
+	cType := Blank
+	if isSample {
+		cType = Sample
+	}
+	return &ContentData{
+		Name:     name,
+		Resource: cResource,
+		Type:     cType,
+	}
 }
