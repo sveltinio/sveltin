67c69a217f3d58cb0309309b8368d62e8adf23a9
Mirco Veltri <github@mircoveltri.me>
2022-11-22 17:33:35 +0100

    chore: unused code removed

--- common/collections.go
+++ common/collections.go
@@ -8,13 +8,6 @@
 // Package common contains utility functions for collections, maps and filesystem.
 package common
 
-import (
-	"errors"
-	"strconv"
-
-	sveltinerr "github.com/sveltinio/sveltin/internal/errors"
-)
-
 // Contains returns true if an element is in a slice.
 func Contains(s []string, str string) bool {
 	for _, v := range s {
@@ -68,45 +61,4 @@ func Union(a, b []string) []string {
 		}
 	}
 	return Unique(a)
-}
-
-// CheckMinMaxArgs returns an error if the number of args is not within the expected range.
-func CheckMinMaxArgs(items []string, min int, max int) error {
-	switch numOfArgs := len(items); {
-	case numOfArgs < min:
-		errA := errors.New(`This command expects at least ` + strconv.Itoa(min) + ` argument.
-Please check the help: sveltin [command] -h`)
-		return sveltinerr.NewNumOfArgsNotValidErrorWithMessage(errA)
-	case (numOfArgs >= min) && (numOfArgs <= max):
-		return nil
-	case numOfArgs > max:
-		errA := errors.New(`This command expects maximum ` + strconv.Itoa(max) + ` arguments.
-Please check the help: sveltin [command] -h`)
-		return sveltinerr.NewNumOfArgsNotValidErrorWithMessage(errA)
-	default:
-		errA := errors.New("")
-		return sveltinerr.NewDefaultError(errA)
-	}
-}
-
-// CheckMaxArgs returns an error if there are more than N args.
-func CheckMaxArgs(items []string, max int) error {
-	var errorMsg string
-	if max == 0 {
-		errorMsg = "This command expects no arguments. Please check the help: sveltin [command] -h"
-	} else {
-		errorMsg = `This command expects maximum ` + strconv.Itoa(max) + ` arguments.
-Please check the help: sveltin [command] -h`
-	}
-
-	switch numOfArgs := len(items); {
-	case (numOfArgs >= 1) && (numOfArgs <= max):
-		return nil
-	case numOfArgs > max:
-		errA := errors.New(errorMsg)
-		return sveltinerr.NewNumOfArgsNotValidErrorWithMessage(errA)
-	default:
-		errA := errors.New(errorMsg)
-		return sveltinerr.NewDefaultError(errA)
-	}
 }
