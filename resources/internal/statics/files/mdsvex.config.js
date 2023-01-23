18fbdb5871136c34c2a5a3d17ce6a05379c2bffe
Mirco Veltri <github@mircoveltri.me>
2023-01-23 14:59:36 +0100

    chore: replace headings with @sveltinio/remark-headings, remark-slug removed

--- resources/internal/statics/files/mdsvex.config.js
+++ resources/internal/statics/files/mdsvex.config.js
@@ -2,28 +2,31 @@ import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 import relativeImages from 'mdsvex-relative-images';
 
 import emoji from 'remark-emoji';
-import remarkSlug from 'remark-slug';
 import readingTime from 'remark-reading-time';
 import rehypeAutoLinkHeadings from 'rehype-autolink-headings';
 import rehypeExternalLinks from 'rehype-external-links';
 import rehypeSlug from 'rehype-slug';
-import headings from './src/lib/utils/headings.js';
+import headings from '@sveltinio/remark-headings';
 
 const mdsvexConfig = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
 	smartypants: {
-		dashes: 'oldschool'
+		dashes: 'oldschool',
 	},
-	remarkPlugins: [remarkSlug, headings, emoji, readingTime(), relativeImages],
+	remarkPlugins: [headings, emoji, readingTime(), relativeImages],
 	rehypePlugins: [
-		[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
-		rehypeSlug[
+		[
+			(rehypeExternalLinks,
+			{ target: '_blank', rel: ['noopener', 'noreferrer'] }),
+		],
+		rehypeSlug,
+		[
 			(rehypeAutoLinkHeadings,
 			{
-				behavior: 'wrap'
-			})
-		]
-	]
+				behavior: 'wrap',
+			}),
+		],
+	],
 });
 
 export default mdsvexConfig;
