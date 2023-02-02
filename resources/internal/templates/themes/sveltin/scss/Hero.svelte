4033228242dac5813dca5dbc3cf0fbaa09e83c36
Mirco Veltri <github@mircoveltri.me>
2023-02-02 22:32:14 +0100

    refactor(scss/sveltin): hero component structure, tuning css

--- resources/internal/templates/themes/sveltin/scss/Hero.svelte
+++ resources/internal/templates/themes/sveltin/scss/Hero.svelte
@@ -2,73 +2,48 @@
 	import { Link } from '@sveltinio/essentials';
 </script>
 
-<section class="hero">
-	<div class="content">
-		<div class="wrapper">
-			<div class="subwrapper">
-				<div class="herotext">
-					<h1>
-						Your Sveltin project is <span class="accent"
-							>up & running!</span
-						>
-					</h1>
+<section>
+	<h1>
+		Your Sveltin project is <span class="accent">up & running!</span>
+	</h1>
 
-					<div class="buttons-wrapper">
-						<Link
-							id="quickStartLink"
-							external
-							icon={false}
-							href="https://docs.sveltin.io/quick-start"
-							class="hero-btn hero-btn-accent">Quick Start</Link
-						>
+	<div class="buttons-wrapper">
+		<Link
+			id="quickStartLink"
+			external
+			icon={false}
+			href="https://docs.sveltin.io/quick-start"
+			class="hero-btn hero-btn--accent">Quick Start</Link
+		>
 
-						<Link
-							id="documentationLink"
-							external
-							icon={false}
-							href="https://docs.sveltin.io"
-							class="hero-btn hero-btn-base">Read the Docs</Link
-						>
-					</div>
-				</div>
-			</div>
-		</div>
+		<Link
+			id="documentationLink"
+			external
+			icon={false}
+			href="https://docs.sveltin.io"
+			class="hero-btn hero-btn--base">Read the Docs</Link
+		>
 	</div>
 </section>
 
 <style lang="scss">
-	.hero > .content {
-		max-width: 80rem;
+	section {
+		max-width: 80rem /* 1280px */;
 		margin-left: auto;
 		margin-right: auto;
-
-		> .wrapper {
-			padding-top: 11rem;
-			padding-bottom: 11rem;
-		}
+		padding-top: 11rem /* 176px */;
+		padding-bottom: 11rem /* 176px */;
+		padding-left: 1.5rem /* 24px */;
+		padding-right: 1.5rem /* 24px */;
 	}
 
-	.subwrapper {
-		max-width: 64rem;
-		padding-left: 1.5rem;
-		padding-right: 1.5rem;
-		margin-left: auto;
-		margin-right: auto;
-	}
-
-	.herotext {
-		padding-bottom: 6rem;
+	h1 {
+		font-size: 1.875rem /* 30px */;
+		font-weight: 700;
 		text-align: center;
-
-		h1 {
-			font-size: 1.875rem;
-			font-weight: 700;
-			text-align: center;
-			letter-spacing: -0.05em;
-			padding-bottom: 1rem;
-			line-height: 1.25;
-			$color: $dark;
-		}
+		letter-spacing: -0.05em;
+		padding-bottom: 1rem /* 16px */;
+		line-height: 1.25;
 	}
 
 	.accent {
@@ -86,23 +61,77 @@
 		width: 100%;
 		margin-left: auto;
 		margin-right: auto;
-		margin-top: 1rem;
+		margin-top: 1rem /* 16px */;
 	}
 
 	@media (min-width: 640px) {
-		.herotext h1 {
-			font-size: 2.25rem;
+		h1 {
+			font-size: 2.25rem /* 36px */;
 		}
 	}
 
 	@media (min-width: 1024px) {
-		.subwrapper {
-			padding-left: 4rem;
-			padding-right: 4rem;
+		h1 {
+			font-size: 3rem /* 48px */;
+		}
+	}
+
+	:global {
+		.hero-btn {
+			position: relative;
+			display: inline-flex;
+			align-items: center;
+			padding-top: 0.5rem /* 8px */;
+			padding-right: 0.5rem /* 8px */;
+			padding-bottom: 0.5rem /* 8px */;
+			padding-left: 0.5rem /* 8px */;
+			font-size: 0.875rem /* 14px */;
+			font-weight: 500;
+			border-width: 2px;
+			border-style: solid;
 		}
 
-		.herotext h1 {
-			font-size: 3rem;
+		.hero-btn--base {
+			margin-left: -1px;
+			color: $dark;
+			border-top-right-radius: 0.5rem /* 8px */;
+			border-bottom-right-radius: 0.5rem /* 8px */;
+			border-color: $color-text-base;
+
+			&:focus {
+				z-index: 10;
+				outline: 2px solid transparent;
+				outline-offset: 2px;
+			}
+		}
+
+		.hero-btn--accent {
+			color: $white;
+			background-color: $color-text-base;
+			border-top-left-radius: 0.5rem /* 8px */;
+			border-bottom-left-radius: 0.5rem /* 8px */;
+			border-color: $color-text-base;
+
+			&:hover {
+				color: $white;
+				background-color: $dark;
+			}
+
+			&:focus {
+				z-index: 10;
+				outline: 2px solid transparent;
+				outline-offset: 2px;
+				border-color: $white;
+			}
+		}
+
+		@media only screen and (min-width: 1024px) {
+			.hero-btn {
+				padding-top: 0.5rem /* 8px */;
+				padding-right: 0.75rem /* 8px */;
+				padding-bottom: 0.5rem /* 8px */;
+				padding-left: 0.75rem /* 8px */;
+			}
 		}
 	}
 </style>
