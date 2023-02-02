b98e40624f8739f3126d4270e09abea8112a063e
Mirco Veltri <github@mircoveltri.me>
2023-02-02 21:56:28 +0100

    refactor(bootstrap/sveltin): hero component structure, tuning css

--- resources/internal/templates/themes/sveltin/bootstrap/Hero.svelte
+++ resources/internal/templates/themes/sveltin/bootstrap/Hero.svelte
@@ -2,76 +2,136 @@
 	import { Link } from '@sveltinio/essentials';
 </script>
 
-<section class="d-flex justify-content-center text-center">
-	<div class="mx-auto mw-7xl">
-		<div class="mx-auto w-full py-44">
-			<div class="subwrapper mx-auto">
-				<div class="pb-24 text-center">
-					<h1 class="pb-4 mx-auto fw-bold text-center lh-sm">
-						Your Sveltin project is <span
-							class="text-teal block text-capitalize fst-italic fw-normal"
-							>up & running!</span
-						>
-					</h1>
+<section>
+	<h1>
+		Your Sveltin project is <span class="accent">up & running!</span>
+	</h1>
 
-					<div
-						class="mx-auto mt-4 d-inline-flex justify-content-center btn-group"
-						role="group"
-					>
-						<Link
-							id="quickStartLink"
-							external
-							icon={false}
-							href="https://docs.sveltin.io/quick-start"
-							class="hero-btn hero-btn-accent">Quick Start</Link
-						>
+	<div class="buttons-wrapper">
+		<Link
+			id="quickStartLink"
+			external
+			icon={false}
+			href="https://docs.sveltin.io/quick-start"
+			class="hero-btn hero-btn--accent">Quick Start</Link
+		>
 
-						<Link
-							id="documentationLink"
-							external
-							icon={false}
-							href="https://docs.sveltin.io"
-							class="hero-btn hero-btn-base">Read the Docs</Link
-						>
-					</div>
-				</div>
-			</div>
-		</div>
+		<Link
+			id="documentationLink"
+			external
+			icon={false}
+			href="https://docs.sveltin.io"
+			class="hero-btn hero-btn--base">Read the Docs</Link
+		>
 	</div>
 </section>
 
 <style lang="scss">
-	h1 {
-		font-size: 1.875rem;
-		letter-spacing: -0.05em;
+	section {
+		max-width: 80rem /* 1280px */;
+		margin-left: auto;
+		margin-right: auto;
+		padding-top: 11rem /* 176px */;
+		padding-bottom: 11rem /* 176px */;
+		padding-left: 1.5rem /* 24px */;
+		padding-right: 1.5rem /* 24px */;
 	}
 
-	.subwrapper {
-		width: 22rem;
-		padding-left: 1.5rem;
-		padding-right: 1.5rem;
+	h1 {
+		font-size: 1.875rem /* 30px */;
+		font-weight: 700;
+		text-align: center;
+		letter-spacing: -0.05em;
+		padding-bottom: 1rem /* 16px */;
+		line-height: 1.25;
+	}
+
+	.accent {
+		color: $color-text-accent;
+		font-style: italic;
+		font-weight: 400;
+		display: block;
+		text-transform: capitalize;
+	}
+
+	.buttons-wrapper {
+		position: relative;
+		display: inline-flex;
+		justify-content: center;
+		width: 100%;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 1rem /* 16px */;
 	}
 
 	@media (min-width: 640px) {
 		h1 {
-			font-size: 2.25rem;
-		}
-		.subwrapper {
-			width: 30rem;
-			padding-left: 4rem;
-			padding-right: 4rem;
+			font-size: 2.25rem /* 36px */;
 		}
 	}
 
 	@media (min-width: 1024px) {
 		h1 {
-			font-size: 3rem;
+			font-size: 3rem /* 48px */;
+		}
+	}
+
+	:global {
+		.hero-btn {
+			position: relative;
+			display: inline-flex;
+			align-items: center;
+			padding-top: 0.5rem /* 8px */;
+			padding-right: 0.5rem /* 8px */;
+			padding-bottom: 0.5rem /* 8px */;
+			padding-left: 0.5rem /* 8px */;
+			font-size: 0.875rem /* 14px */;
+			font-weight: 500;
+			border-width: 2px;
+			border-style: solid;
 		}
 
-		.subwrapper {
-			width: 38rem;
-			padding-left: 4rem;
-			padding-right: 4rem;
+		.hero-btn--base {
+			margin-left: -1px;
+			color: $dark;
+			border-top-right-radius: 0.5rem /* 8px */;
+			border-bottom-right-radius: 0.5rem /* 8px */;
+			border-color: $color-text-base;
+
+			&:focus {
+				z-index: 10;
+				outline: 2px solid transparent;
+				outline-offset: 2px;
+			}
+		}
+
+		.hero-btn--accent {
+			color: $white;
+			background-color: $color-text-base;
+			border-top-left-radius: 0.5rem /* 8px */;
+			border-bottom-left-radius: 0.5rem /* 8px */;
+			border-color: $color-text-base;
+
+			&:hover {
+				color: $white;
+				background-color: $dark;
+			}
+
+			&:focus {
+				z-index: 10;
+				outline: 2px solid transparent;
+				outline-offset: 2px;
+				border-color: $white;
+			}
+		}
+
+		@media only screen and (min-width: 1024px) {
+			.hero-btn {
+				padding-top: 0.5rem /* 8px */;
+				padding-right: 0.75rem /* 8px */;
+				padding-bottom: 0.5rem /* 8px */;
+				padding-left: 0.75rem /* 8px */;
+			}
 		}
 	}
 </style>
