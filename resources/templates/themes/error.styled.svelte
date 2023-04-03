4dc34c7da0cdeb54a765a0fb633bb12582bc0ba3
Mirco Veltri <github@mircoveltri.me>
2023-04-03 09:59:44 +0200

    style(+error.svelte): sveltin theme

--- resources/templates/themes/error.styled.svelte
+++ resources/templates/themes/error.styled.svelte
@@ -5,40 +5,29 @@
 <svelte:head>
 	<title>{$page.status}: {$page.error.message}</title>
 </svelte:head>
-<div class="error-container">
-	<div class="wrapper">
-		<main class="main">
-			<p class="error-code">{$page.status}</p>
-			<div class="message-wrapper">
-				<div class="message-main">
-					<h1 class="error-title">
-						{$page.error.message}
-					</h1>
-					<p class="error-text">
-						Please check the URL in the address bar and try again.
-					</p>
-				</div>
-			</div>
-		</main>
+
+<div class="error-wrapper">
+	<p class="error-code">{$page.status}</p>
+	<div class="error-message">
+		<h1 class="error-title">
+			{$page.error.message}
+		</h1>
+		<p class="error-text">
+			Please check the URL in the address bar and try again.
+		</p>
 	</div>
 </div>
 
 <style>
-	.error-container {
+	.error-wrapper {
 		min-height: 100%;
 		padding-top: 4rem;
 		padding-right: 1rem;
 		padding-bottom: 4rem;
 		padding-left: 1rem;
-	}
-
-	.wrapper {
 		max-width: max-content;
 		margin-left: auto;
 		margin-right: auto;
-	}
-
-	.main {
 		justify-content: center;
 		align-items: center;
 	}
@@ -70,15 +59,12 @@
 			font-size: 3rem;
 			line-height: 1;
 		}
-		.error-container {
+		.error-wrapper {
+			display: flex;
 			padding-left: 1.5rem;
 			padding-right: 1.5rem;
 			padding-top: 6rem;
 			padding-bottom: 6rem;
-		}
-
-		.main {
-			display: flex;
 		}
 
 		.error-code {
@@ -86,11 +72,8 @@
 			line-height: 1;
 		}
 
-		.message-wrapper {
+		.error-message {
 			margin-left: 1.5rem;
-		}
-
-		.message-main {
 			border-style: solid;
 			border-top-width: 0;
 			border-right-width: 0;
@@ -102,14 +85,14 @@
 	}
 
 	@media (min-width: 768) {
-		.error-container {
+		.error-wrapper {
 			display: grid;
 			place-items: center;
 		}
 	}
 
 	@media (min-width: 1024px) {
-		.error-container {
+		.error-wrapper {
 			padding-left: 2rem;
 			padding-right: 2rem;
 		}
